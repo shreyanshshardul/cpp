146c0a3b6694ab129b578002bd7926382f1884ee
shreyanshvillian <118564071+shreyanshvillian@users.noreply.github.com>
2024-05-12 12:33:25 +0530

    traversal of array.

---
+++ traversalofarray.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+}
