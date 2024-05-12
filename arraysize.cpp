b493a9ddc7838f6fa66cfc3ffcae1416a61b284b
shreyanshvillian <118564071+shreyanshvillian@users.noreply.github.com>
2024-05-12 12:17:53 +0530

    array size

---
+++ arraysize.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[]={1,2,3,4,5};
+    //for calculating total space occupied by array
+    cout<<sizeof(a)<<endl;
+    //for calculating array size or no. of element in array
+    cout<<sizeof(a)/sizeof(a[1]);
+    return 0;
+}
