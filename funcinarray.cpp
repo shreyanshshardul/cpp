28f5e10047f8a06f2139605f9381e6308a401e66
shreyanshvillian <118564071+shreyanshvillian@users.noreply.github.com>
2024-05-17 02:02:02 +0530

    function in array

---
+++ funcinarray.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int a[] , int size){
+    for(int i=0;i<size;i++){
+        cout<< a[i]<<" "<<" ";
+    }
+}
+int main(){
+int ab[5]={1,2,3,4,5};
+print(ab,5);
+int cv[5]={5,4,3,2,1};
+print(cv,5);
+return 0;
+}
